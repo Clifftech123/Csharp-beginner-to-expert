75899f7c8cc6771ee39b51f96b65aa7fae96fd37
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2024-05-07 14:40:11 +0000

    Updating the code

---
+++ Module-1/Lesson_1/Readme.md
@@ -0,0 +1,3 @@
+# Lesson 1: What is C#?
+
+ With section doesn't contain any code so just refer to the video for the content.
---
+++ Module-1/Lesson_3/Program.cs
@@ -0,0 +1,4 @@
+﻿
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Press any key to exit.");
+Console.WriteLine("This is a new line of code.");
