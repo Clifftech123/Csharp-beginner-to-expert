26fa9ff01f16fd3b602744cefa1b163839d0f5af
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2024-06-16 01:53:02 +0000

    Changing  the leeson name

--- lesson4/Program.cs
+++ lesson4/Program.cs
@@ -1,142 +1,90 @@
 ﻿
-// Addition  ("+") :  5 + 3 = 8
+// Welcome to lesson 4 of the C# tutorial, in this lesson we are going to learn about variables in C#
 
-int a = 40; 
-int b = 30;
-int ab = a + b;
+
+// Working numbers in C#
+
+
+
+ //  single line comment
+
+ /// <summary>
+ ///  multi line comment
+ /// </summary>
+ 
+int age = 30 ;
+Console.WriteLine("My age is " + age);
+
+
+// int 
+int Some ;
+Some = 20;
+Console.WriteLine(Some);
+
+
+// float
+
+float height = 1.75f;
+Console.WriteLine("My height is " + height);
+
+
+
+// double 
+double weight = 70.5;
+Console.WriteLine("My weight is " + weight);
+
+
+
+//  String types 
+
+string name = "Clifford";
+Console.WriteLine("My name is " + name);
+
+
+Console.WriteLine("=======================================================  Variable Declaration and Initialization ");
+
+
+// Variable Declaration and Initialization
+
+
+int a = 10;  // Declaring and initializing in one line
+int b = 20;
+int ab ;
+ab = 20;
+
+
+Console.WriteLine("The value of a is " + a);
+Console.WriteLine("The value of b is " + b);
 Console.WriteLine("The value of ab is " + ab);
 
 
-// Subtraction  ("-") :  5 - 3 = 2
+Console.WriteLine("=======================================================  Naming conventions ");
 
-int c = 40;
-int d = 30;
+// Naming conventions
 
-int cd = c - d;
-Console.WriteLine("The value of cd is " + cd);
+int myAge = 30; // Camel Case
+int MyAge = 30; // Pascal Case
 
+// int 1age = 30; // Cannot start with a number
 
+// int my-age = 30; // Cannot use a hyphen
 
+// int my age = 30; // Cannot use a space
 
-// Multiplication  ("*") :  5 * 3 = 15
-
-int e = 5;
-int f = 10;
-
-int ef = e * f;
-Console.WriteLine("The value of ef is " + ef);
+// int my_age = 30; // Use an underscore
 
 
-// Division  ("/") :  5 / 3 = 1
-
-
-int g = 7;
-int h = 3;
-int gh = g / h;
-
-Console.WriteLine("The value of gh is " + gh);
+int myAge1 = 30; 
+ double myAgeInYears = myAge1 ;
+Console.WriteLine("My age in years is " + myAgeInYears);
 
 
 
-// Modulus  ("%") :  5 % 3 = 2
-
-int i = 7;
-int j = 3;
-int ij = i % j;
-
-
-// Increment  ("++") :  5++ = 6
-// Decrement  ("--") :  5-- = 4
-
-
-int k = 6;
-k++; // increment by 1
-
-Console.WriteLine("The value of k is " + k);
-
- int KB = 6 ;
- KB -- ; // decrement by 1
-
-Console.WriteLine("The value of KB is " + KB);
+// Constants 
+const  string MYfirst  = "Clifford is here ";
+Console.WriteLine(MYfirst);
 
 
 
-//  Relational Operators 
-
-//  Equal to (==) :  5 == 3 = False
-
-int l = 10;
-int m = 5;
-bool lm = l == m;
-
-Console.WriteLine("The value of lm is " + lm);
 
 
-//  Not Equal to (!=) :  5 != 3 = True
-
-
-int n = 10;
-int o = 10;
-
-bool no = n != o;
-
-Console.WriteLine("The value of no is " + no);
-
-
-//  Greater than (>) :  5 > 3 = True
-
-int p = 10;
-int q = 5;
-bool pq = p > q;
-Console.WriteLine("The value of pq is " + pq);
-
-
-//  Less than (<) :  5 < 3 = False
-
-int r = 10;
-int s = 5;
-
-bool rs = r < s;
-
-Console.WriteLine("The value of rs is " + rs);
-
-
-
-// Logical Operators
-
-//  AND (&&) :  5 > 3 && 5 < 10 = True
-
-int t = 10;
-int u = 5;
-int v = 10;
-bool tuv = t > u && t < v;
-Console.WriteLine("The value of tuv is " + tuv);
-
-
-// OR (||) :  5 > 3 || 5 < 10 = True
-
-int w = 10;
-int x = 5;
-int y = 10;
-
-bool wxy = w > x || w < y;
-Console.WriteLine("The value of wxy is " + wxy);
-
-
-
-// NOT (!) :  !(5 > 3) = False
-
-int z = 10;
-int aa = 10;
- bool zaa = !(z > aa);
-Console.WriteLine("The value of zaa is " + zaa);
-
-
-// Assignment Operators
-
-// - Assignment (`=`)
-// - Addition assignment (`+=`)
-// - Subtraction assignment (`-=`)
-// - Multiplication assignment (`*=`)
-// - Division assignment (`/=`)
-// - Modulus assignment (`%=`)
---
+++ lesson5/Program.cs
@@ -0,0 +1,142 @@
+﻿
+// Addition  ("+") :  5 + 3 = 8
+
+int a = 40; 
+int b = 30;
+int ab = a + b;
+Console.WriteLine("The value of ab is " + ab);
+
+
+// Subtraction  ("-") :  5 - 3 = 2
+
+int c = 40;
+int d = 30;
+
+int cd = c - d;
+Console.WriteLine("The value of cd is " + cd);
+
+
+
+
+// Multiplication  ("*") :  5 * 3 = 15
+
+int e = 5;
+int f = 10;
+
+int ef = e * f;
+Console.WriteLine("The value of ef is " + ef);
+
+
+// Division  ("/") :  5 / 3 = 1
+
+
+int g = 7;
+int h = 3;
+int gh = g / h;
+
+Console.WriteLine("The value of gh is " + gh);
+
+
+
+// Modulus  ("%") :  5 % 3 = 2
+
+int i = 7;
+int j = 3;
+int ij = i % j;
+
+
+// Increment  ("++") :  5++ = 6
+// Decrement  ("--") :  5-- = 4
+
+
+int k = 6;
+k++; // increment by 1
+
+Console.WriteLine("The value of k is " + k);
+
+ int KB = 6 ;
+ KB -- ; // decrement by 1
+
+Console.WriteLine("The value of KB is " + KB);
+
+
+
+//  Relational Operators 
+
+//  Equal to (==) :  5 == 3 = False
+
+int l = 10;
+int m = 5;
+bool lm = l == m;
+
+Console.WriteLine("The value of lm is " + lm);
+
+
+//  Not Equal to (!=) :  5 != 3 = True
+
+
+int n = 10;
+int o = 10;
+
+bool no = n != o;
+
+Console.WriteLine("The value of no is " + no);
+
+
+//  Greater than (>) :  5 > 3 = True
+
+int p = 10;
+int q = 5;
+bool pq = p > q;
+Console.WriteLine("The value of pq is " + pq);
+
+
+//  Less than (<) :  5 < 3 = False
+
+int r = 10;
+int s = 5;
+
+bool rs = r < s;
+
+Console.WriteLine("The value of rs is " + rs);
+
+
+
+// Logical Operators
+
+//  AND (&&) :  5 > 3 && 5 < 10 = True
+
+int t = 10;
+int u = 5;
+int v = 10;
+bool tuv = t > u && t < v;
+Console.WriteLine("The value of tuv is " + tuv);
+
+
+// OR (||) :  5 > 3 || 5 < 10 = True
+
+int w = 10;
+int x = 5;
+int y = 10;
+
+bool wxy = w > x || w < y;
+Console.WriteLine("The value of wxy is " + wxy);
+
+
+
+// NOT (!) :  !(5 > 3) = False
+
+int z = 10;
+int aa = 10;
+ bool zaa = !(z > aa);
+Console.WriteLine("The value of zaa is " + zaa);
+
+
+// Assignment Operators
+
+// - Assignment (`=`)
+// - Addition assignment (`+=`)
+// - Subtraction assignment (`-=`)
+// - Multiplication assignment (`*=`)
+// - Division assignment (`/=`)
+// - Modulus assignment (`%=`)
